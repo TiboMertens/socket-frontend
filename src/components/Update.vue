2b10e519bd80172d42d4c438d1ee3eaf27a1e5b3
TiboMertens <tibomertens25@gmail.com>
2023-11-28 17:22:51 +0100

    feat: add teams & score to update page

--- src/components/Update.vue
+++ src/components/Update.vue
@@ -1,8 +1,24 @@
 <script setup>
+import { ref, reactive, onMounted } from 'vue';
+let teams = reactive(["Anderlecht", "Antwerp", "OHL"]);
+let selectedTeam = ref(null);
+
+let score = ref(null);
+
+const updateStats = () => {
+};
+
 </script>
 
 <template>
     <h1>Update stats</h1>
+    <label for="teamSelect">Select Team:</label>
+    <select v-model="selectedTeam" id="teamSelect">
+      <option v-for="team in teams" :key="team" :value="team">{{ team }}</option>
+    </select>
+    <input type="text" v-model="score">
+    <button @click="updateStats">Update</button>
+    
 </template>
   
 <style scoped></style>
