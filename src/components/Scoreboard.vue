ae2f9fcdaf54920f9835d7e24611d6004476f38e
TiboMertens <tibomertens25@gmail.com>
2023-11-28 18:52:22 +0100

    feat: display team & score on scoreboard

--- src/components/Scoreboard.vue
+++ src/components/Scoreboard.vue
@@ -1,8 +1,23 @@
 <script setup>
+import { ref, reactive, onMounted } from 'vue';
+
+let data = reactive([{
+    "team": "Cercle Brugge",
+    "score": "26"
+},
+{
+    "team": "KAA Gent",
+    "score": "28"
+}]);
 </script>
 
 <template>
     <h1>Scoreboard</h1>
+    <ul>
+        <li v-for="item in data" :key="item.team">
+            {{ item.team }}: {{ item.score }}
+        </li>
+    </ul>
 </template>
   
 <style scoped></style>
